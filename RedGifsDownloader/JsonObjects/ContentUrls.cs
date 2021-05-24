80266f401e014950cf86b05587495036c13dcb0a
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2021-05-24 21:00:06 +0200

    Create ContentUrls.cs

---
+++ RedGifsDownloader/JsonObjects/ContentUrls.cs
@@ -0,0 +1,16 @@
+﻿namespace RedGifsDownloader.JsonObjects
+{
+    public class ContentUrls
+    {
+        public Mp4 mp4 { get; set; }
+        public Max2mbGif max2mbGif { get; set; }
+        public Max5mbGif max5mbGif { get; set; }
+        public _100pxGif _100pxGif { get; set; }
+        public LargeGif largeGif { get; set; }
+        public Mobile mobile { get; set; }
+        public MobilePoster mobilePoster { get; set; }
+        public Webp webp { get; set; }
+        public Max1mbGif max1mbGif { get; set; }
+        public Webm webm { get; set; }
+    }
+}
