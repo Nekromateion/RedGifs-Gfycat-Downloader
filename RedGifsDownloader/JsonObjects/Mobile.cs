5bf8bace582bcb06bde7afc3c5faeef55604adff
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2021-05-24 21:00:19 +0200

    Create Mobile.cs

---
+++ RedGifsDownloader/JsonObjects/Mobile.cs
@@ -0,0 +1,10 @@
+﻿namespace RedGifsDownloader.JsonObjects
+{
+    public class Mobile
+    {
+        public int width { get; set; }
+        public int size { get; set; }
+        public string url { get; set; }
+        public int height { get; set; }
+    }
+}
