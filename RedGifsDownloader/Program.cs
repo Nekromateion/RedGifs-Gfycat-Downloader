7be6dd32a2e9345ffe4eaec1bf1803b2553c8f98
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2021-05-24 21:00:47 +0200

    Update Program.cs

--- RedGifsDownloader/Program.cs
+++ RedGifsDownloader/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace RedGifsDownloader
+﻿using System;
+
+namespace RedGifsDownloader
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.Write("Please enter the id of the user you want to download: ");
+            string id = Console.ReadLine().ToLower();
+            Console.Write("Do you want to download the mp4s(type mp4) or gifs(type gif): ");
+            bool downloadMp4 = Console.ReadLine() == "mp4";
+            Console.Write("What is the minimum amount of likes a gif/video should have? ");
+            int minLikes = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                RedGifs.DownloadUser(id, downloadMp4, minLikes);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something failed wile downloading...");
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
         }
     }
 }
