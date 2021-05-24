99a560a7e4b7cac69362cce22908822408ab2bdb
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2021-05-24 21:00:30 +0200

    Create User.cs

---
+++ RedGifsDownloader/JsonObjects/User.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RedGifsDownloader.JsonObjects
+{
+    public class User
+    {
+        public string cursor { get; set; }
+        public List<Gfycat> gfycats { get; set; }
+    }
+}
