69a0afe950baf6b59856006fc800543b5504ad59
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2021-05-26 04:04:23 +0200

    Update Program.cs

--- RedGifsDownloader/Program.cs
+++ RedGifsDownloader/Program.cs
@@ -7,37 +7,35 @@ namespace RedGifsDownloader
         public static void Main(string[] args)
         {
             Console.Write("Do you want to download by user(type 1) or by search term(2)");
-            string inp = Console.ReadLine();
+            var inp = Console.ReadLine();
             Console.Write(inp == "1"
                 ? "Please enter the id of the user you want to download: "
                 : "Please enter the search term you want to download: ");
-            string input = Console.ReadLine().ToLower();
+            var input = Console.ReadLine().ToLower();
             Console.Title = inp == "1" ? "Downloading User: " + input : "Downloading Search: " + input;
             Console.Write("Do you want to download the mp4s(type mp4) or gifs(type gif): ");
-            bool downloadMp4 = Console.ReadLine() == "mp4";
+            var downloadMp4 = Console.ReadLine() == "mp4";
             Console.Write("What is the minimum amount of likes a gif/video should have? ");
-            int minLikes = Convert.ToInt32(Console.ReadLine());
+            var minLikes = Convert.ToInt32(Console.ReadLine());
             try
             {
                 if (inp == "1")
-                {
                     RedGifs.DownloadUser(input, downloadMp4, minLikes);
-                }
                 else
-                {
                     RedGifs.DownloadBySearch(input, downloadMp4, minLikes);
-                }
             }
             catch (Exception e)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Something failed wile downloading...");
+                Console.WriteLine(
+                    "Something failed wile downloading...\nPlease report this on the github repo (https://github.com/Nekromateion/RedGifsDownloader)");
                 Console.WriteLine(e);
             }
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("DOWNLOAD FINISHED!");
-            Console.Title = inp == "1" ? "FINISHED Downloading User: " + input : "FINISHED Downloading Search: " + input;
+            Console.Title =
+                inp == "1" ? "FINISHED Downloading User: " + input : "FINISHED Downloading Search: " + input;
             Console.ReadLine();
         }
     }
