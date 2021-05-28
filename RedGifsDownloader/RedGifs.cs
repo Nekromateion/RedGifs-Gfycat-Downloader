204a2f1b6174de0b394e3ac6b24fcfc23cf91193
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2021-05-28 16:04:24 +0200

    Switch to JObject to fix deserialization errors
    Switched all JsonConvert.Deserialize<> to JObjects to fix deserialization errors.

--- RedGifsDownloader/RedGifs.cs
+++ RedGifsDownloader/RedGifs.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
-using Newtonsoft.Json;
-using RedGifsDownloader.JsonObjects;
+using Newtonsoft.Json.Linq;
 
 namespace RedGifsDownloader
 {
@@ -13,67 +12,70 @@ namespace RedGifsDownloader
         internal static void DownloadUser(string userId, bool downloadMp4, int minLikes)
         {
             Directory.CreateDirectory(userId);
-            var user = JsonConvert.DeserializeObject<ApiResponse>(
-                WebClient.DownloadString($"{Api.BaseUrl}{Api.UsersEndpoint}{userId}{Api.UserGfysEndpoint}?count=100"));
+            var user = JObject.Parse(WebClient.DownloadString($"{Api.BaseUrl}{Api.UsersEndpoint}{userId}{Api.UserGfysEndpoint}?count=100"));
+            //var user = JsonConvert.DeserializeObject<ApiResponse>(WebClient.DownloadString($"{Api.BaseUrl}{Api.UsersEndpoint}{userId}{Api.UserGfysEndpoint}?count=100"));
             if (user != null)
             {
-                foreach (var gif in user.gfycats)
-                    if (gif.likes >= minLikes)
+                foreach (var gif in user["gfycats"])
+                {
+                    if ((int)gif["likes"] >= minLikes)
                     {
-                        Console.WriteLine($"Downloading {gif.gfyName}\t({gif.views} views & {gif.likes} likes)");
+                        Console.WriteLine($"Downloading {(string)gif["gfyName"]}\t({(string)gif["views"]} views & {(string)gif["likes"]} likes)");
                         if (downloadMp4)
                         {
                             try
                             {
-                                WebClient.DownloadFile(gif.mp4Url ?? gif.mobileUrl,
-                                    userId + "/" + gif.gfyName + ".mp4");
+                                WebClient.DownloadFile((string)gif["mp4Url"] ?? (string)gif["mobileUrl"],
+                                    userId + "/" + (string)gif["gfyName"] + ".mp4");
                             }
                             catch
                             {
-                                WebClient.DownloadFile(gif.mobileUrl, userId + "/" + gif.gfyName + ".mp4");
+                                WebClient.DownloadFile((string)gif["mobileUrl"], userId + "/" + (string)gif["gfyName"] + ".mp4");
                             }
                         }
                         else
                         {
-                            if (gif.content_urls != null && gif.content_urls.largeGif != null &&
-                                gif.content_urls.largeGif.url != null)
-                                WebClient.DownloadFile(gif.content_urls.largeGif.url,
-                                    userId + "/" + gif.gfyName + ".gif");
-                            else if (gif.gifUrl != null)
-                                WebClient.DownloadFile(gif.gifUrl, userId + "/" + gif.gfyName + ".gif");
+                            if (gif["content_urls"] != null && gif["content_urls"]["largeGif"] != null &&
+                                gif["content_urls"]["largeGif"]["url"] != null)
+                                WebClient.DownloadFile((string)gif["content_urls"]["largeGif"]["url"],
+                                    userId + "/" + (string)gif["gfyName"] + ".gif");
+                            else if (gif["gifUrl"] != null)
+                                WebClient.DownloadFile((string)gif["gifUrl"], userId + "/" + (string)gif["gfyName"] + ".gif");
                         }
-                    }
+                    }   
+                }
 
-                while (user.cursor != null)
+                while ((string)user["cursor"] != null)
                 {
-                    user = JsonConvert.DeserializeObject<ApiResponse>(WebClient.DownloadString(
-                        $"{Api.BaseUrl}{Api.UsersEndpoint}{userId}{Api.UserGfysEndpoint}?count=100&cursor={user.cursor}"));
-                    foreach (var gif in user.gfycats)
-                        if (gif.likes >= minLikes)
+                    user = JObject.Parse(WebClient.DownloadString($"{Api.BaseUrl}{Api.UsersEndpoint}{userId}{Api.UserGfysEndpoint}?count=100&cursor={(string)user["cursor"]}"));
+                    foreach (var gif in user["gfycats"])
+                    {
+                        if ((int)gif["likes"] >= minLikes)
                         {
-                            Console.WriteLine($"Downloading {gif.gfyName}\t({gif.views} views & {gif.likes} likes)");
+                            Console.WriteLine($"Downloading {(string)gif["gfyName"]}\t({(string)gif["views"]} views & {(string)gif["likes"]} likes)");
                             if (downloadMp4)
                             {
                                 try
                                 {
-                                    WebClient.DownloadFile(gif.mp4Url ?? gif.mobileUrl,
-                                        userId + "/" + gif.gfyName + ".mp4");
+                                    WebClient.DownloadFile((string)gif["mp4Url"] ?? (string)gif["mobileUrl"],
+                                        userId + "/" + (string)gif["gfyName"] + ".mp4");
                                 }
                                 catch
                                 {
-                                    WebClient.DownloadFile(gif.mobileUrl, userId + "/" + gif.gfyName + ".mp4");
+                                    WebClient.DownloadFile((string)gif["mobileUrl"], userId + "/" + (string)gif["gfyName"] + ".mp4");
                                 }
                             }
                             else
                             {
-                                if (gif.content_urls != null && gif.content_urls.largeGif != null &&
-                                    gif.content_urls.largeGif.url != null)
-                                    WebClient.DownloadFile(gif.content_urls.largeGif.url,
-                                        userId + "/" + gif.gfyName + ".gif");
-                                else if (gif.gifUrl != null)
-                                    WebClient.DownloadFile(gif.gifUrl, userId + "/" + gif.gfyName + ".gif");
+                                if (gif["content_urls"] != null && gif["content_urls"]["largeGif"] != null &&
+                                    gif["content_urls"]["largeGif"]["url"] != null)
+                                    WebClient.DownloadFile((string)gif["content_urls"]["largeGif"]["url"],
+                                        userId + "/" + (string)gif["gfyName"] + ".gif");
+                                else if (gif["gifUrl"] != null)
+                                    WebClient.DownloadFile((string)gif["gifUrl"], userId + "/" + (string)gif["gfyName"] + ".gif");
                             }
-                        }
+                        }   
+                    }
                 }
             }
             else
@@ -85,71 +87,63 @@ namespace RedGifsDownloader
         internal static void DownloadBySearch(string searchTerm, bool downloadMp4, int minLikes)
         {
             Directory.CreateDirectory("search/" + searchTerm);
-            var search = JsonConvert.DeserializeObject<ApiResponse>(
-                WebClient.DownloadString(
-                    $"{Api.BaseUrl}{Api.SearchEndpoint}?search_text={searchTerm}&count=150&order=trending"));
+            var search = JObject.Parse(WebClient.DownloadString($"{Api.BaseUrl}{Api.SearchEndpoint}?search_text={searchTerm}&count=150&order=trending"));
             if (search != null)
             {
-                foreach (var gif in search.gfycats)
-                    if (gif.likes >= minLikes)
+                foreach (var gif in search["gfycats"])
+                {
+                    if ((int)gif["likes"] >= minLikes)
                     {
-                        Console.WriteLine($"Downloading {gif.gfyName}\t({gif.views} views & {gif.likes} likes)");
+                        Console.WriteLine($"Downloading {(string)gif["gfyName"]}\t({(string)gif["views"]} views & {(string)gif["likes"]} likes)");
                         if (downloadMp4)
                         {
                             try
                             {
-                                WebClient.DownloadFile(gif.mp4Url ?? gif.mobileUrl,
-                                    "search/" + searchTerm + "/" + gif.gfyName + ".mp4");
+                                WebClient.DownloadFile((string)gif["mp4Url"] ?? (string)gif["mobileUrl"], "search/" + searchTerm + "/" + (string)gif["gfyName"] + ".mp4");
                             }
                             catch
                             {
-                                WebClient.DownloadFile(gif.mobileUrl,
-                                    "search/" + searchTerm + "/" + gif.gfyName + ".mp4");
+                                WebClient.DownloadFile((string)gif["mobileUrl"], "search/" + searchTerm + "/" + (string)gif["gfyName"] + ".mp4");
                             }
                         }
                         else
                         {
-                            if (gif.content_urls != null && gif.content_urls.largeGif != null &&
-                                gif.content_urls.largeGif.url != null)
-                                WebClient.DownloadFile(gif.content_urls.largeGif.url,
-                                    "search/" + searchTerm + "/" + gif.gfyName + ".gif");
-                            else if (gif.gifUrl != null)
-                                WebClient.DownloadFile(gif.gifUrl, "search/" + searchTerm + "/" + gif.gfyName + ".gif");
+                            if (gif["content_urls"] != null && gif["content_urls"]["largeGif"] != null && gif["content_urls"]["largeGif"]["url"] != null)
+                                WebClient.DownloadFile((string)gif["content_urls"]["largeGif"]["url"], "search/" + searchTerm + "/" + (string)gif["gfyName"] + ".gif");
+                            else if (gif["gifUrl"] != null)
+                                WebClient.DownloadFile((string)gif["gifUrl"],  "search/" + searchTerm + "/" + (string)gif["gfyName"] + ".gif");
                         }
-                    }
+                    }   
+                }
 
-                while (search.cursor != null)
+                while ((string)search["cursor"] != null)
                 {
-                    search = JsonConvert.DeserializeObject<ApiResponse>(WebClient.DownloadString(
-                        $"{Api.BaseUrl}{Api.SearchEndpoint}?search_text={searchTerm}&count=150&order=trending&cursor={search.cursor}"));
-                    foreach (var gif in search.gfycats)
-                        if (gif.likes >= minLikes)
+                    search = JObject.Parse(WebClient.DownloadString($"{Api.BaseUrl}{Api.SearchEndpoint}?search_text={searchTerm}&count=150&order=trending&cursor={(string)search["cursor"]}"));
+                    foreach (var gif in search["gfycats"])
+                    {
+                        if ((int)gif["likes"] >= minLikes)
                         {
-                            Console.WriteLine($"Downloading {gif.gfyName}\t({gif.views} views & {gif.likes} likes)");
+                            Console.WriteLine($"Downloading {(string)gif["gfyName"]}\t({(string)gif["views"]} views & {(string)gif["likes"]} likes)");
                             if (downloadMp4)
                             {
                                 try
                                 {
-                                    WebClient.DownloadFile(gif.mp4Url ?? gif.mobileUrl,
-                                        "search/" + searchTerm + "/" + gif.gfyName + ".mp4");
+                                    WebClient.DownloadFile((string)gif["mp4Url"] ?? (string)gif["mobileUrl"], "search/" + searchTerm + "/" + (string)gif["gfyName"] + ".mp4");
                                 }
                                 catch
                                 {
-                                    WebClient.DownloadFile(gif.mobileUrl,
-                                        "search/" + searchTerm + "/" + gif.gfyName + ".mp4");
+                                    WebClient.DownloadFile((string)gif["mobileUrl"], "search/" + searchTerm + "/" + (string)gif["gfyName"] + ".mp4");
                                 }
                             }
                             else
                             {
-                                if (gif.content_urls != null && gif.content_urls.largeGif != null &&
-                                    gif.content_urls.largeGif.url != null)
-                                    WebClient.DownloadFile(gif.content_urls.largeGif.url,
-                                        "search/" + searchTerm + "/" + gif.gfyName + ".gif");
-                                else if (gif.gifUrl != null)
-                                    WebClient.DownloadFile(gif.gifUrl,
-                                        "search/" + searchTerm + "/" + gif.gfyName + ".gif");
+                                if (gif["content_urls"] != null && gif["content_urls"]["largeGif"] != null && gif["content_urls"]["largeGif"]["url"] != null)
+                                    WebClient.DownloadFile((string)gif["content_urls"]["largeGif"]["url"], "search/" + searchTerm + "/" + (string)gif["gfyName"] + ".gif");
+                                else if (gif["gifUrl"] != null)
+                                    WebClient.DownloadFile((string)gif["gifUrl"],  "search/" + searchTerm + "/" + (string)gif["gfyName"] + ".gif");
                             }
-                        }
+                        }   
+                    }
                 }
             }
             else
